c0f1bed3c00764e11119593696227b3144460c78
ahmedqadar <abdulnet029@gmail.com>
2023-12-12 15:41:10 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram(str):
+    def __init__(self, word):
+        self.word = word
+
+    def is_anagram(self, word):
+        return (
+            self.word.lower() != word.lower() and
+            sorted(self.word.lower()) == sorted(word.lower())
+        )
+
+    def match(self, candidates):
+        return [word for word in candidates if self.is_anagram(word)]
